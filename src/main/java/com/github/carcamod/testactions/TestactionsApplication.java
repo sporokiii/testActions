9ff378e7254814c9e33a0daacd9c645607590c68
CarcamoD <davidcarcamorigau98@gmail.com>
2020-03-28 10:43:11 +0100

    First commit

---
+++ src/main/java/com/github/carcamod/testactions/TestactionsApplication.java
@@ -0,0 +1,13 @@
+package com.github.carcamod.testactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestactionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/carcamod/testactions/TestactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.carcamod.testactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
