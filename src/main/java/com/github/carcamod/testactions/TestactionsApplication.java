d9308e5eb37b9a278c37fc842cd4345eae7f7053
CarcamoD <davidcarcamorigau98@gmail.com>
2020-03-29 10:05:08 +0100

    Test

--- src/main/java/com/github/carcamod/testactions/TestactionsApplication.java
+++ src/main/java/com/github/carcamod/testactions/TestactionsApplication.java
@@ -8,7 +8,7 @@ public class TestactionsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestactionsApplication.class, args);
-        System.out.println("Pull request test");
+        System.out.println("Pull request testt");
     }
 
 }
